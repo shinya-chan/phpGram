994eed55a303751c3bdc8ca54742c5c4eb48123a
Iman Mokhtari Aski <imanskater@outlook.com>
2019-01-24 17:37:07 +0330

    This is Imans Commit at 5:36

--- app/Traits/ForwardMessage.php
+++ app/Traits/ForwardMessage.php
@@ -5,21 +5,23 @@ namespace App\Traits;
 
 use App\interfaces\Send;
 
-class ForwardMessage implements Send {
+class ForwardMessage implements Send
+{
 
     public $msg;
 
-    public function send () {
+    use Execute;
 
-    }
-
-    public function prepare ( array $arguments) {
+    public function prepare(array $arguments)
+    {
         $message = ([
             'chat_id' => $arguments['chat_id'],
             'from_chat_id' => $arguments['from_chat_id'],
-            'disable_notification' => $arguments['disable_notification'] ?? 'false',
-            'message_id' => 'message_id'
+            $arguments['disable_notification'] ?? 'disable_notification' => $arguments['disable_notification'],
+            'message_id' => $arguments['message_id'],
+            $arguments['reply_markup'] ?? 'reply_markup' =>  $arguments['reply_markup']
         ]);
+
         $this->msg = json_encode($message);
     }
 
